f9e3a9312320776f27363586f4cb9b6fb8e12e49
Leonardo <josue.aguilar1995@gmail.com>
2022-04-28 19:47:09 -0500

    pseudo code in script.js

---
+++ assests/js/script.js
@@ -0,0 +1,20 @@
+// Pseudo code for Trevor
+
+/*
+
+https://api.open5e.com/magicitems/
+
+pull from the dnd api
+
+pull item description
+
+organize description
+
+set up object based on the playing card
+
+create element with data-deck and a 13- or 22-card class
+
+
+
+
+*/
