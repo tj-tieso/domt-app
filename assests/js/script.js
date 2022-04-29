5807618246f7e458fae85308e55d36aecb5ecb7e
Leonardo <josue.aguilar1995@gmail.com>
2022-04-28 19:55:40 -0500

    more pseudo code

--- assests/js/script.js
+++ assests/js/script.js
@@ -10,11 +10,23 @@ pull item description
 
 organize description
 
-set up object based on the playing card
+set up object based on the playing card (only 13 or 22 cards based on selection)
 
 create element with data-deck and a 13- or 22-card class
 
+append element to DOM based on selected card draw
 
+click to flip, only on hidden. Do nothing on click when card is already shown.
+
+leave ability to summon modal again for details
+
+save number of cards drawn and which cards are flipped to localStorage
+
+when card amount is selected, then we loop through n number numbers
+
+check figure for too many or too little cards selected (1-13/1-22)
+
+reset button to be able to draw again
 
 
 */
